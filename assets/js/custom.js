0ef44df50c68e51c5c6d8005ba014ee466182440
Prakhar Varshney <prakharvarshney28@gmail.com>
2015-11-11 02:05:51 +0000

    New Changes
    Prakhar

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#nav ul li').on('click', function(eve, ele) {
+ 			var moduleBackColor = $($($(this).find('a').attr('href')).parent()).css('background-color');
+ 			$('#nav').css('background-color',moduleBackColor);
+ 			$('#nav a').css('background', 'transparent');
+ 			$(this).find('a').css('background', '#383838');
+
+ 			// if($(this).find('a').hasClass('liAgenda')) {
+ 			// 	$('#nav').addClass('navWithLogo');
+ 			// }
+ 			// else{
+ 			// 	$('#nav').removeClass('navWithLogo');	
+ 			// }
+		
+	});
+});
